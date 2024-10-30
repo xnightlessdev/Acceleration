f7960b7ae266f6b1f2f8e876ac2796c2b0a81fc2
xdevnightless <142658833+xdevnightless@users.noreply.github.com>
2024-10-30 09:57:25 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,26 @@
-document.getElementById("searchform").addEventListener("submit", async function(event) { event.preventDefault(); try { await registerSW(); // Calls service worker registration const searchbar = document.getElementById("searchbar"); const url = search(searchbar.value, "https://www.google.com/search?q=%s"); window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url); } catch (err) { alert(err.toString()); } }); /** * @param {string} input * @param {string} template Template for a search query. * @returns {string} Fully qualified URL */ function search(input, template) { try { return new URL(input).toString(); } catch (err) { try { const url = new URL(`http://${input}`); if (url.hostname.includes(".")) return url.toString(); } catch (err) {} } return template.replace("%s", encodeURIComponent(input));
+"use strict";
+/**
+ *
+ * @param {string} input
+ * @param {string} template Template for a search query.
+ * @returns {string} Fully qualified URL
+ */
+function search(input, template) {
+  try {
+    // input is a valid URL:
+    // eg: https://example.com, https://example.com/test?q=param
+    return new URL(input).toString();
+  } catch (err) {
+    // input was not a valid URL
+  }
+
+  try {
+    // input is a valid URL when http:// is added to the start:
+    // eg: example.com, https://example.com/test?q=param
+    const url = new URL(`http://${input}`);
+    // only if the hostname has a TLD/subdomain
+    if (url.hostname.includes(".")) return url.toString();
+  } catch (err) {
+  }
+
+

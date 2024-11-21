daece21d00a55e4f8bb337ea8cca40311f876a90
Nightless <142658833+xdevnightless@users.noreply.github.com>
2024-11-21 10:58:43 -0600

    Delete now.gg directory

--- now.gg/bypass.js
+++
@@ -1,9 +0,0 @@
-alert("bypass by Potato. https://github.com/dragon731012");
-setInterval(() => {
-    var div=document.getElementsByTagName("div");
-    for (var i=0; i<div.length;i++){
-        if (div[i].className.includes("ad-blocker")){
-            div[i].style.display="none";
-        }
-    }
-},0);
--- now.gg/nowgg.js
+++
@@ -1,5 +0,0 @@
-var doc = document.getElementById("iframe").contentWindow; 
-  
-var script=doc.document.createElement("script");
-script.src=window.location.origin+"/nowgg/bypass.js";
-doc.document.body.appendChild(script);
